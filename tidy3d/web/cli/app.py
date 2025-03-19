f578bfc363dc91bcd89bda7d113ea6e4c01d79ab
Yannick Augenstein <yannick@flexcompute.com>
2025-03-19 17:29:58 +0100

    fix config

--- tidy3d/web/cli/app.py
+++ tidy3d/web/cli/app.py
@@ -74,7 +74,7 @@ def configure_fn(apikey: str) -> None:
                 return
 
     if not apikey:
-        current_apikey = config.apikey or ""
+        current_apikey = config.auth.apikey or ""
         message = f"Current API key: [{current_apikey}]\n" if current_apikey else ""
         apikey = click.prompt(f"{message}Please enter your api key", type=str)
 
@@ -89,11 +89,11 @@ def configure_fn(apikey: str) -> None:
         click.echo("Configured successfully.")
 
         # Update the config with the new API key
-        config.apikey = apikey
+        config.auth.apikey = apikey
 
         # If a legacy config already exists, save to that location to maintain compatibility
         legacy_path = config.CONFIG_PATHS["legacy"]
-        if legacy_path.exists():
+        if legacy_path.exists() or config.CONFIG_PATHS["old_legacy"].exists():
             config.save(legacy_path)
             click.echo(f"Configuration saved to {legacy_path}")
         else:
